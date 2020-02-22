0e2a879257454e191ddf3dc548896e2647b4b09a
MarkWh1te <iamwh1temark@outlook.com>
2020-02-22 18:27:26 +0800

    Finished Maze Generate Part

--- main.js
+++ main.js
@@ -1,60 +1,119 @@
+const cellNumber = 30
+
 var canvas = e("#canvas")
 var ctx = canvas.getContext("2d")
 const mazeSize = canvas.width
-const cellNumber = 40
 
-const getTestMatrix = () => {
-    let matrix = []
-    for (let i = 0; i < cellNumber; i++) {
-        let column = []
-        for (let j = 0; j < cellNumber; j++) {
-            column.push(randomInt(2))
+let mazeGenerateId = 0
+let matrix = getTestMatrix()
+let frontier = []
+frontier = addFrontier(0, 0, 0, 0, frontier, matrix)
+
+
+const mark = (x, y, frontier, matrix) => {
+    frontier = addFrontier(x, y, x - 1, y, frontier, matrix)
+    frontier = addFrontier(x, y, x + 1, y, frontier, matrix)
+    frontier = addFrontier(x, y, x, y + 1, frontier, matrix)
+    frontier = addFrontier(x, y, x, y - 1, frontier, matrix)
+    return frontier
+}
+
+
+
+const generateMaze = (matrix) => {
+    let startX = randomInt(cellNumber)
+    let startY = randomInt(cellNumber)
+    let frontier = []
+    // frontier = addFrontier(startX, startY, startX, startY, frontier, matrix)
+    frontier = addFrontier(0, 0, 0, 0, frontier, matrix)
+    while (frontier.length > 0) {
+        let cells = frontier.splice(randomInt(frontier.length), 1)[0]
+        let toCellX = cells[2]
+        let toCellY = cells[3]
+        if (matrix[toCellY][toCellX] == 1) {
+            matrix[toCellY][toCellX] = 0
+            matrix[cells[0]][cells[1]] = 0
+            // console.log(matrix, toCellY, toCellX, matrix[toCellX][toCellY])
+            // frontier = mark(toCellX, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX - 1, toCellY, toCellX - 2, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX + 1, toCellY, toCellX + 2, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX, toCellY - 1, toCellX, toCellY - 2, frontier, matrix)
+            frontier = addFrontier(toCellX, toCellY + 1, toCellX, toCellY + 2, frontier, matrix)
         }
-        matrix.push(column)
     }
     return matrix
 }
 
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+const update = () => {
+    if (frontier.length > 0) {
+        let cells = frontier.splice(randomInt(frontier.length), 1)[0]
+        let toCellX = cells[2]
+        let toCellY = cells[3]
+        if (matrix[toCellY][toCellX] == 1) {
+            matrix[toCellY][toCellX] = 0
+            matrix[cells[0]][cells[1]] = 0
+            // console.log(matrix, toCellY, toCellX, matrix[toCellX][toCellY])
+            // frontier = mark(toCellX, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX - 1, toCellY, toCellX - 2, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX + 1, toCellY, toCellX + 2, toCellY, frontier, matrix)
+            frontier = addFrontier(toCellX, toCellY - 1, toCellX, toCellY - 2, frontier, matrix)
+            frontier = addFrontier(toCellX, toCellY + 1, toCellX, toCellY + 2, frontier, matrix)
+        }
+    } else {
+        stopMazeGenerateAnimation()
+    }
 }
 
-function draw() {
-    // empty origin canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let matrix = getTestMatrix()
-    drawTable(matrix)
-}
 
+// TODO:change name
 const drawTable = (matrix) => {
     const cellSize = mazeSize / cellNumber
-    info(cellSize)
     for (let i = 0; i < matrix.length; i++) {
         const column = matrix[i];
         for (let j = 0; j < column.length; j++) {
             const cell = column[j];
             if (cell == 1) {
-                ctx.fillStyle = "#FF0000";
+                ctx.fillStyle = "black"
+                // ctx.fillStyle = "#FF0000";
             } else {
-                ctx.fillStyle = "#228B22";
+                ctx.fillStyle = "white"
+                // ctx.fillStyle = "#228B22";
             }
             ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+            if ((i == 0 && j == 0) || (j == (cellNumber - 2) && i == (cellNumber - 2))) {
+                ctx.fillStyle = "red"
+                ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+            }
         }
     }
 }
 
+function stopMazeGenerateAnimation() {
+    window.clearInterval(mazeGenerateId)
+    mazeGenerateId = 0
+    info("Maze Generation Finished")
+}
 
+const mazeGenerateLoop = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    update()
+    drawTable(matrix)
+}
+
+const startMazeGenerate = () => {
+    // Check Before Start
+    if (mazeGenerateLoop != 0) {
+        stopMazeGenerateAnimation()
+    }
+    mazeGenerateId = window.setInterval(mazeGenerateLoop, 1);
+}
+
+const findPathLoop = () => {
+
+}
 
 const main = () => {
-
-    // set up loop, with 60 fps
-    setInterval(draw, 1000 / 60);
-    // draw()
+    startMazeGenerate()
 }
 
 main()
--- util.js
+++ util.js
@@ -4,3 +4,23 @@ const e = selector => document.querySelector(selector)
 const randomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+const getTestMatrix = () => {
+    let matrix = []
+    for (let i = 0; i < cellNumber; i++) {
+        let column = []
+        for (let j = 0; j < cellNumber; j++) {
+            // column.push(randomInt(2))
+            column.push(1)
+        }
+        matrix.push(column)
+    }
+    return matrix
+}
+
+const addFrontier = (x1, y1, x2, y2, frontier, matrix) => {
+    if (x1 >= 0 && y1 >= 0 && x1 < cellNumber && y1 < cellNumber && x2 >= 0 && y2 >= 0 && x2 < cellNumber && y2 < cellNumber && (matrix[y2][x2] == 1 || matrix[y1][x1] == 1)) {
+        frontier.push([x1, y1, x2, y2])
+    }
+    return frontier
+}
